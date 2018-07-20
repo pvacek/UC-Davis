d3673a6828d306c4cf25cd7fc4e0bbf5ebe9d6f9
Patrick Vacek <prvacek@ucdavis.edu>
2018-07-20 00:44:10 -0700

    Add files via upload

--- Upwork/Permit/textscrape.R
+++ Upwork/Permit/textscrape.R
@@ -3,11 +3,6 @@ library(pdftools)
 
 #Scrape by text in the best way possible
 
-same_highway<-function(route_df){
-  dupes<-which(diff(as.numeric(factor(route_df$route)))==0)
-  return(route_df[-dupes,])
-}
-
 text_scrape<-function(pdf,state=""){
   raw<-pdf_read(pdf)
   lean<-gsub("[ ]{2,}"," ",gsub("[^0-9 A-z]","",raw))
@@ -18,6 +13,5 @@ text_scrape<-function(pdf,state=""){
   route_df<-data.frame(miles=NA,route=c("",routes,""),distance=NA,est_time=NA,orienation=c(NA,or,NA),
                        type=c("Stateline",rep("Highway",n),"Stateline"))
   route_df$route<-route_clean(route_df$route,state)
-  route_df<-same_highway(route_df)
   return(route_df)
 }
